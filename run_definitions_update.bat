f37ed9edd737f836ea9ab70865fa7c6b24538f1c
ptvrdy <peyton.tvrdy123@gmail.com>
2025-03-19 15:30:04 -0400

    Made filepath relative

--- run_definitions_update.bat
+++ run_definitions_update.bat
@@ -1,3 +1,3 @@
-@echo off
-"C:\Program Files\Git\bin\bash.exe" --login -i "C:/source/repos/dcat-us-v1.1-parser/update_definitions.sh"
+@echo on
+"C:\Program Files\Git\bin\bash.exe" --login -i "./update_definitions.sh"
 pause
